b0f782d12bbb11448a7eec6b02d1d84599983ded
hachazo <87842940+hachazo@users.noreply.github.com>
2023-04-30 00:34:19 -0300

    Update

---
+++ lista.ads
@@ -0,0 +1,32 @@
+generic
+   
+   type Tipoelemento is private;
+   
+   package Lista is
+      
+      type Tipolista is private;
+         
+         function Vacia(Lista: Tipolista) return Boolean;
+         function Esta(Lista: Tipolista; Elem: Tipoelemento) return Boolean;
+     
+         procedure Insertar(Lista: in out Tipolista; Elem: in Tipoelemento);
+         procedure Insertarfinal(Lista: in out Tipolista; Elem: in Tipoelemento);
+         procedure Suprimir(Lista: in out Tipolista; Elem: Tipoelemento);
+         procedure Limpiar(Lista: in out Tipolista);
+         
+         function Info(Lista: Tipolista) return Tipoelemento;
+         function Sig(Lista: Tipolista) return Tipolista;
+         
+         Listavacia: exception;
+         
+         private
+            
+         type tiponodo;
+         type Tipolista is access Tiponodo;
+         
+         type Tiponodo is record
+            Info: Tipoelemento;
+            Sig: Tipolista;
+         end record;
+         
+      end lista;
