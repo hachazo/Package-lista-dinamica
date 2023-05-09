4e924e642bd9f564185faf8bc1cbdce5fa1d79b5
hachazo <87842940+hachazo@users.noreply.github.com>
2023-05-09 00:19:18 -0300

    Update

--- lista.ads
+++ lista.ads
@@ -10,13 +10,13 @@ generic
          function Esta(Lista: Tipolista; Elem: Tipoelemento) return Boolean;
 		 
 		 procedure Crear (Lista: out TipoLista);
-         procedure Insertar(Lista: in out Tipolista; Elem: in Tipoelemento);
-		 procedure Insertarfinal(Lista:in out Tipolista; Elem: in Tipoelemento);
+         procedure Insertar(Lista: in out Tipolista; Elem: in Tipoelemento); -- Al imprimir se muesta el ultimo elemento ingresado primero.
+		 procedure Insertarfinal(Lista:in out Tipolista; Elem: in Tipoelemento);  -- Al imprimir se muestra el primer elemento ingresado primero.
          procedure Suprimir(Lista: in out Tipolista; Elem: Tipoelemento);
          procedure Limpiar(Lista: in out Tipolista);
          
-         function Info(Lista: Tipolista) return Tipoelemento;
-         function Sig(Lista: Tipolista) return Tipolista;
+         function Info(Lista: Tipolista) return Tipoelemento; -- Imprime el elemento de la lista segun fue ingresado.
+         function Sig(Lista: Tipolista) return Tipolista; -- Imprime el siguiente elemento de la lista.
          
          Listavacia: exception;
          
